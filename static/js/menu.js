39fcefa3ef527efbd2990338a25af4230aaef9e4
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-04-05 20:30:19 -0300

    add sw and manifest

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,28 +1,27 @@
 const floatButton = document.querySelector(".floating-btn");
 const circularMenu = document.querySelector("#circularMenu");
-const openHamburgerMenu = document.querySelector('#openHamburgerMenu');
-const closeHamburgerMenu = document.querySelector('#closeHamburgerMenu');
-const openModal = document.querySelector('#openModal');
-const closeComboMenu = document.querySelector('#closeComboMenu');
-
+const openHamburgerMenu = document.querySelector("#openHamburgerMenu");
+const closeHamburgerMenu = document.querySelector("#closeHamburgerMenu");
+const openModal = document.querySelector("#openModal");
+const closeComboMenu = document.querySelector("#closeComboMenu");
 
 floatButton.addEventListener("click", () => {
   floatButton.classList.toggle("pulsed");
   circularMenu.classList.toggle("active");
 });
 
-openHamburgerMenu.addEventListener('click', () => {
+openHamburgerMenu.addEventListener("click", () => {
   document.querySelector("#myNav").style.height = "100%";
 });
 
-closeHamburgerMenu.addEventListener('click', () => {
+closeHamburgerMenu.addEventListener("click", () => {
   document.querySelector("#myNav").style.height = "0%";
 });
 
-openModal.addEventListener('click', () => {
+openModal.addEventListener("click", () => {
   document.querySelector("#myCombo").style.height = "100%";
 });
 
-closeComboMenu.addEventListener('click', () => {
+closeComboMenu.addEventListener("click", () => {
   document.querySelector("#myCombo").style.height = "0%";
 });
